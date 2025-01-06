6fef65e9a1aae3a9c92f7e5bc71857f3edcde4fa
yjr <emilyryoo5307@gmail.com>
2025-01-06 23:55:07 +0900

    v2

--- src/main/java/jpabook/jpashop_v2/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop_v2/api/MemberApiController.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
@@ -16,13 +17,11 @@ public class MemberApiController {
     private final MemberService memberService;
 
     /*
-     * 등록 V2
+     * 등록 V1: 요청 값으로 Member 엔티티 직접 받음
      */
 
     @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(
-            @RequestBody @Valid Member member
-            ){
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long id=memberService.join(member);
         return new CreateMemberResponse(id);
     }
@@ -40,4 +39,9 @@ public class MemberApiController {
             this.id=id;
         }
     }
+
+    /*
+    수정 API
+     */
+    @PutMapping("/api/v2/members/{id}")
 }
