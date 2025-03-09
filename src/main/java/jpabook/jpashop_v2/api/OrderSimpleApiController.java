4b6a408ae5672904b513424494634dc70f29b19b
yjr <emilyryoo5307@gmail.com>
2025-03-09 23:50:34 +0900

    feat: OrderAPIController V1

---
+++ src/main/java/jpabook/jpashop_v2/api/OrderSimpleApiController.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop_v2.api;
+
+/*
+ * 유저 서비스 로직 -> API Controller 로 묶기 추천
+ */
+/*
+ * xToOne(ManyToOne, OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+
+import jpabook.jpashop_v2.domain.Order;
+import jpabook.jpashop_v2.domain.OrderSearch;
+import jpabook.jpashop_v2.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    /*
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처ㅣ
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기환
+        }
+        return all;
+    }
+}
